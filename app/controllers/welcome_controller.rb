363f64abf03022fe5311857654c8381f273bfb54
Chase Childers <24755648+chase-childers@users.noreply.github.com>
2019-06-29 10:12:33 -0700

    Add resume_data for display, add basic css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @data = RESUME_DATA
   end
 end
---
+++ config/initializers/resume_data.rb
@@ -0,0 +1,3 @@
+require 'yaml'
+
+RESUME_DATA = YAML::load_file(File.join(__dir__, 'resume_data.yml'))
