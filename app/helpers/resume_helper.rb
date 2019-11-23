19fc94cf9ffd3cabdc1e036e1e5919943798cd3d
Chase Childers <24755648+chase-childers@users.noreply.github.com>
2019-11-22 17:43:57 -0800

    Update S3 Client for Staging

--- app/helpers/resume_helper.rb
+++ app/helpers/resume_helper.rb
@@ -13,7 +13,7 @@ module ResumeHelper
         end
 
         if @@resume_data.nil? || Time.now > @@next_pull
-            resp = S3_CLIENT.get_object({bucket:'chase-childers', key:'resume_data.yml'})
+            resp = S3_CLIENT.get_object({bucket:S3_INFO[:bucket], key:S3_INFO[:file]})
             if resp.etag != @@resume_hash
                 @@resume_data = YAML::load(resp.body.string)
                 @@resume_hash = resp.etag
--- config/initializers/s3_client.rb
+++ config/initializers/s3_client.rb
@@ -1,5 +1,11 @@
 require 'aws-sdk-s3'
 
+
+S3_INFO = {
+    :bucket => ENV.fetch("BUCKET_NAME") {"rails-resume"},
+    :file => ENV.fetch("RESUME_FILE") {"resume_data.yml"}
+}
+
 S3_CLIENT = Aws::S3::Client.new(
     access_key_id: Rails.application.credentials.aws[:access_key_id],
     secret_access_key: Rails.application.credentials.aws[:secret_access_key],
